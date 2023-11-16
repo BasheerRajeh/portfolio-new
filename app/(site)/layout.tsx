2b07d0e3cb9e5afeeeaed4d952d811fbf90b8b60
Basheer Rajeh <muhammadbasheerrajeh@hotmail.com>
2023-11-16 21:59:00 +0300

    style: add scroll-area to main layout

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import Svg from '@/components/common/svg'
 import Header from '@/components/header'
+import { ScrollArea } from '@/components/ui/scroll-area'
 
 type SiteLayoutProps = {
     children: React.ReactNode
@@ -9,12 +10,14 @@ type SiteLayoutProps = {
 
 const SiteLayout: React.FC<SiteLayoutProps> = ({ children }) => {
     return (
-        <div className='h-screen'>
+        <div className='h-screen overflow-hidden'>
             <Svg.noise />
             <Header className='fixed inset-x-0 top-4 z-40 mx-auto h-[60px] max-w-5xl' />
-            <main className='relative mx-auto mb-16 max-w-5xl px-6 py-24 sm:px-8'>
-                {children}
-            </main>
+            <ScrollArea>
+                <main className='relative mx-auto h-screen max-w-5xl px-6 py-24 sm:px-8'>
+                    {children}
+                </main>
+            </ScrollArea>
         </div>
     )
 }
