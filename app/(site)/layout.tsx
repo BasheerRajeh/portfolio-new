25be978bde3dae25b0cb12612d33192149c06ae0
Basheer Rajeh <muhammadbasheerrajeh@hotmail.com>
2023-11-17 03:08:21 +0300

    style: add gradient background

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import React from 'react'
 
 import Svg from '@/components/common/svg'
@@ -12,6 +13,14 @@ const SiteLayout: React.FC<SiteLayoutProps> = ({ children }) => {
     return (
         <div className='h-screen overflow-hidden'>
             <Svg.noise />
+            <Image
+                width={1512}
+                height={550}
+                className='absolute left-1/2 top-0 -z-10 -translate-x-1/2'
+                src='/images/gradient-background-top.png'
+                alt=''
+                priority
+            />
             <Header className='fixed inset-x-0 top-4 z-40 mx-auto h-[60px] max-w-5xl' />
             <ScrollArea>
                 <main className='relative mx-auto h-screen max-w-5xl px-6 py-24 sm:px-8'>
